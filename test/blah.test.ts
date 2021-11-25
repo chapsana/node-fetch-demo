82df4eb4efd1d45d91e5e5e91399b1daeff52572
Alpha Olomi <alphaolomi@gmail.com>
2021-11-25 03:40:08 +0300

    Update blah.test.ts

--- test/blah.test.ts
+++ test/blah.test.ts
@@ -7,6 +7,32 @@ describe('blah', () => {
   });
 });
 
+describe('Some logging behavior', () => {
+  const log = console.log; // save original console.log function
+  beforeEach(() => {
+    console.log = jest.fn(); // create a new mock function for each test
+  });
+  afterAll(() => {
+    console.log = log; // restore original console.log after all tests
+  });
+  test('no log', () => {
+    // TODO: test something that should not log
+    expect(console.log).not.toHaveBeenCalled();
+  });
+  test('some log on NODE_ENV=development', () => {
+    process.env.NODE_ENV = 'development';
+    expect(sum(1, 1)).toEqual(2);
+    expect(console.log).toHaveBeenCalled();
+    expect(console.log).toHaveBeenCalledWith('boop');
+  });
+
+  test('no log on NODE_ENV!=development', () => {
+    process.env.NODE_ENV = 'production';
+    expect(sum(1, 1)).toEqual(2);
+    expect(console.log).not.toHaveBeenCalled();
+  });
+});
+
 jest.mock('node-fetch');
 
 describe('fetch-mock test', () => {
