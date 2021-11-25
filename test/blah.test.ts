1e04bbbef649cd573ef1f19cd29ad8e2b6fe05a1
Alpha Olomi <alphaolomi@gmail.com>
2021-11-25 03:23:06 +0300

    fix it up

--- test/blah.test.ts
+++ test/blah.test.ts
@@ -1,26 +1,32 @@
 import { sum, getUser } from '../src';
+import fetch from 'node-fetch';
+import type { Response } from 'node-fetch';
 
 describe('blah', () => {
+
   it('works', () => {
+
     expect(sum(1, 1)).toEqual(2);
+
   });
 
-  it('works true', async () => {
-    await getUser();
-    expect(sum(1, 1)).toEqual(2);
-  });
 });
 
-// jest.mock('node-fetch');
+jest.mock('node-fetch');
 
-// describe('fetch-mock test', () => {
-//     const mockFetch = fetch as jest.MockedFunction<typeof fetch>;
+describe('fetch-mock test', () => {
+  const mockFetch = fetch as jest.MockedFunction<typeof fetch>;
 
-//     it('check fetch mock test', async () => {
-//       const json = jest.fn() as jest.MockedFunction<any>;
-//       json.mockResolvedValue({ status: 200}); //just sample expected json return value
-//       mockFetch.mockResolvedValue({ ok: true, json } as Response); //just sample expected fetch response
-//       await getUser();
-//       expect(json.mock.calls.length).toBe(1);
-//     })
-// })
+  it('check fetch mock test', async () => {
+    
+    const json = jest.fn() as jest.MockedFunction<any>;
+    
+    json.mockResolvedValue({ status: 200 }); //just sample expected json return value
+    
+    mockFetch.mockResolvedValue({ ok: true, json } as Response); //just sample expected fetch response
+    
+    await getUser();
+    
+    expect(json.mock.calls.length).toBe(1);
+  });
+});
