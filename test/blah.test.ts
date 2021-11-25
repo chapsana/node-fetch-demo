9de3f96f5427260a86de2aed966354610148b9a1
Alpha Olomi <alphaolomi@gmail.com>
2021-11-25 03:26:56 +0300

    style: 💄 lint fix

--- test/blah.test.ts
+++ test/blah.test.ts
@@ -1,15 +1,10 @@
 import { sum, getUser } from '../src';
-import fetch from 'node-fetch';
-import type { Response } from 'node-fetch';
+import fetch, { Response } from 'node-fetch';
 
 describe('blah', () => {
-
   it('works', () => {
-
     expect(sum(1, 1)).toEqual(2);
-
   });
-
 });
 
 jest.mock('node-fetch');
@@ -18,15 +13,14 @@ describe('fetch-mock test', () => {
   const mockFetch = fetch as jest.MockedFunction<typeof fetch>;
 
   it('check fetch mock test', async () => {
-    
     const json = jest.fn() as jest.MockedFunction<any>;
-    
+
     json.mockResolvedValue({ status: 200 }); //just sample expected json return value
-    
+
     mockFetch.mockResolvedValue({ ok: true, json } as Response); //just sample expected fetch response
-    
+
     await getUser();
-    
+
     expect(json.mock.calls.length).toBe(1);
   });
 });
